9eb804626e0348148908eb9cad37aab3348e815e
12090113 <47487266+12090113@users.noreply.github.com>
2023-05-22 23:11:47 -0700

    Functioning flipper

--- horse flipper.gd
+++ horse flipper.gd
@@ -1,26 +1,27 @@
 extends Label
 
 @export var prefix: String = "Things: "
-@export var things: int = 0
 @export var prob: float = 0.5
 @export var cost: int = 5
 @export var flippers: int = 0
-var rand=RandomNumberGenerator.new()
+var unicorns: Node
 
 func _on_button_pressed():
-	if things >= cost:
-		things -= cost
+	if unicorns.things >= cost:
+		unicorns.change_things(-cost)
+		flippers += 1
+		text = prefix + str(flippers)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	unicorns = get_node("/root/Control/Unicorn num")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
+	if unicorns.things == cost:
+		owner.visible = true
 
 func _on_timer_timeout():
-	if randf() < prob:
-		things += 1
-	text = prefix + str(things)lace with function body.
+	for i in range(flippers):
+		if randf() < prob:
+			unicorns.change_things(1)
--- things.gd
+++ things.gd
@@ -2,10 +2,12 @@ extends Label
 
 @export var prefix: String = "Things: "
 @export var things: int = 0
-@export var prob: float = 0.5
-var rand=RandomNumberGenerator.new()
+#@export var prob: float = 0.5
+#var rand=RandomNumberGenerator.new()
 
 func _on_button_pressed():
-	if randf() <prob:
-		things += 1
+	change_things(1)
+
+func change_things(num: int):
+	things += num
 	text = prefix + str(things)
