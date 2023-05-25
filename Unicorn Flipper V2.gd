3a0246f48ad0d9292d841552408aeec6239238c4
LoonTonk <93897399+LoonTonk@users.noreply.github.com>
2023-05-25 13:40:06 -0700

    horse flipper v2

--- Unicorn Flipper V2.gd
+++ Unicorn Flipper V2.gd
@@ -2,8 +2,9 @@ extends Label
 
 @export var prefix: String = "Things: "
 @export var prob: float = 0.5
-@export var baseCost: int = 5
+@export var baseCost: int = 10
 var cost: int = baseCost
+@export var successes: int = 0
 @export var flippers: int = 0
 @onready var unicorns: Node = get_node("/root/Control/Unicorn num")
 @onready var box: Node = get_node("../VScrollBar/VBoxContainer")
