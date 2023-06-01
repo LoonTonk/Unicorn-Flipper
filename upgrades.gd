0d66a7ff5cbbc4843ae4f1942e298b0cdf7154d4
LoonTonk <93897399+LoonTonk@users.noreply.github.com>
2023-06-01 13:29:07 -0700

    yes

--- upgrades.gd
+++ upgrades.gd
@@ -1,23 +1,25 @@
-extends Label
+extends Panel
 
 @export var prefix: String = "Probability "
 @export var baseCost: int = 5
 @onready var cost: int = baseCost
-@export var probIncrease: int = 0
+@export var probIncreasers: int = 0
 @onready var unicorns: Node = get_node("/root/Control/Unicorn num")
 @onready var button: Node = get_node("../Button")
+@onready var text: Node = get_node("../Label2")
 var timer = preload("res://timer.tscn")
 @onready var horseMultiplier: int = get_node("/root/Control/Unicorn num").horseMultiplier
 
 func _on_button_pressed():
 	if unicorns.things >= cost:
 		unicorns.change_things(-cost)
-		probIncrease += 2
-		text = prefix + str(probIncrease)
-		cost = baseCost * pow(1.15, probIncrease)
-		button.text = "Cost: " + str(cost)
+		probIncreasers += 1
+		label.text = prefix + str(probIncreasers)
+		cost = baseCost * pow(2, probIncreasers)
+		button.text = "Cost: " + str(cost) + "\ndead Unicorns"
+		visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if unicorns.things == baseCost:
-		owner.visible = true
+	if unicorns.things >= cost/2:
+		visible = true
